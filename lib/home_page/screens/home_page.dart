12f5b5603aa9b619442824d9107712496407d269
matias33pascual <matias33pascual@gmail.com>
2024-04-24 02:48:16 -0300

    solucionado el problema de la perdida de teclado al cambiar el foco

--- lib/home_page/screens/home_page.dart
+++ lib/home_page/screens/home_page.dart
@@ -1,27 +1,19 @@
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
 import 'package:flutter/material.dart';
-import 'package:macro_sync_client/home_page/providers/home_provider.dart';
-import 'package:provider/provider.dart';
-import 'package:web_socket_channel/io.dart';
 
-class HomePage extends StatefulWidget {
+class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
-  State<HomePage> createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  @override
   Widget build(BuildContext context) {
-    IOWebSocketChannel? channel;
+    // IOWebSocketChannel? channel;
 
-    void onPressHandler() {
-      debugPrint("Conectando al servidor . . .");
-      // IOWebSocketChannel.connect("ws://192.168.100.16:433");
-      // channel.sink.add("HOLA DESDE FLUTTER!!");
-    }
+    // void onPressHandler() {
+    //   debugPrint("Conectando al servidor . . .");
+    //   IOWebSocketChannel.connect("ws://192.168.100.16:433");
+    //   channel.sink.add("HOLA DESDE FLUTTER!!");
+    // }
 
     return Scaffold(
       body: Container(
@@ -41,42 +33,82 @@ class CustomForm extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final formKeyIp = GlobalKey<FormState>();
+    final formKeyPort = GlobalKey<FormState>();
+
     return Column(
       children: [
         CustomInputField(
           hintText: "DIRECCION IP",
+          formKey: formKeyIp,
         ),
         SizedBox(height: 10),
         CustomInputField(
           hintText: "PUERTO",
           textInputType: TextInputType.number,
+          formKey: formKeyPort,
         ),
       ],
     );
   }
 }
 
-class CustomInputField extends StatelessWidget {
+class CustomInputField extends StatefulWidget {
   final String hintText;
   final TextInputType textInputType;
+  final GlobalKey formKey;
 
   const CustomInputField({
     Key? key,
     required this.hintText,
     this.textInputType = TextInputType.text,
+    required this.formKey,
   }) : super(key: key);
+
+  @override
+  State<CustomInputField> createState() => _CustomInputFieldState();
+}
+
+class _CustomInputFieldState extends State<CustomInputField> {
+  late FocusNode _focusNode;
+  bool _hasFocus = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _focusNode = FocusNode();
+    _focusNode.addListener(_handleFocusChange);
+  }
+
+  @override
+  void dispose() {
+    _focusNode.removeListener(_handleFocusChange);
+    _focusNode.dispose();
+    super.dispose();
+  }
+
+  _handleFocusChange() {
+    setState(() {
+      _hasFocus = _focusNode.hasFocus;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Container(
       decoration: BoxDecoration(
-          border: Border(
-        left: BorderSide(
-          width: 3,
-          color: Color.fromARGB(255, 255, 238, 0),
-        ),
-      )),
+        border: _hasFocus
+            ? Border(
+                left: BorderSide(
+                  width: 3,
+                  color: Color.fromARGB(255, 255, 238, 0),
+                ),
+              )
+            : null,
+      ),
       child: TextField(
+        key: widget.formKey,
+        focusNode: _focusNode,
         cursorColor: Colors.white,
         cursorWidth: 3,
         style: TextStyle(
@@ -85,13 +117,14 @@ class CustomInputField extends StatelessWidget {
         ),
         maxLines: 1,
         decoration: InputDecoration(
-          hintText: hintText,
+          hintText: widget.hintText,
           hintStyle: TextStyle(
             color: Colors.white60,
             fontWeight: FontWeight.w600,
           ),
           filled: true,
-          fillColor: Color.fromARGB(255, 168, 155, 53),
+          fillColor:
+              _hasFocus ? Color.fromARGB(255, 168, 155, 53) : Colors.black54,
           border: InputBorder.none,
         ),
       ),
