2256c1ee0a453ad37d0f46ae9644fe420ba89a75
matias33pascual <matias33pascual@gmail.com>
2024-04-26 02:29:01 -0300

    maquetando pantalla home

--- lib/home_page/screens/home_page.dart
+++ lib/home_page/screens/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:macro_sync_client/home_page/screens/widgets/exports_widgets.dart';
+import 'package:macro_sync_client/theme/app_theme.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -14,23 +15,89 @@ class HomePage extends StatelessWidget {
     //   channel.sink.add("HOLA DESDE FLUTTER!!");
     // }
 
+    final decorationEffects = AppTheme.decorationEffects;
+    final colors = AppTheme.colors;
+
     return Scaffold(
       body: Container(
-        height: MediaQuery.of(context).size.height,
         color: Colors.black87,
-        padding: const EdgeInsets.symmetric(horizontal: 24),
+        padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 12),
         child: Center(
           child: SingleChildScrollView(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: const [
-                Text("INGRESA LOS DATOS DE"),
-                Text("MACRO SYNC DESKTOP"),
-                SizedBox(height: 10),
-                CustomForm(),
-                SizedBox(height: 10),
-                ConnectButton(),
-              ],
+            child: SizedBox(
+              height: 210,
+              child: Stack(
+                children: [
+                  Positioned(
+                    top: 0,
+                    left: 0,
+                    child: Container(
+                      width: 8,
+                      height: 210,
+                      decoration: BoxDecoration(
+                          border: Border(
+                        left: BorderSide(
+                          color: colors.borderGray,
+                          width: 1,
+                        ),
+                        top: BorderSide(
+                          color: colors.borderGray,
+                          width: 1,
+                        ),
+                        bottom: BorderSide(
+                          color: colors.borderGray,
+                          width: 1,
+                        ),
+                      )),
+                      child: Padding(
+                        padding:
+                            const EdgeInsets.only(left: 2, top: 2, bottom: 2),
+                        child: Container(
+                          decoration: BoxDecoration(
+                            gradient:
+                                decorationEffects.verticalLinearGradientGrey,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 24),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.only(bottom: 10),
+                          child: Container(
+                            width: MediaQuery.of(context).size.width,
+                            padding: const EdgeInsets.only(bottom: 4),
+                            decoration: BoxDecoration(
+                                border: Border(
+                                    bottom: BorderSide(
+                              color: colors.borderGray,
+                              width: 1,
+                            ))),
+                            child: const Text(
+                              "INGRESA LOS DATOS DE MACRO SYNC DESKTOP",
+                              style: TextStyle(
+                                color: Colors.white,
+                              ),
+                            ),
+                          ),
+                        ),
+                        const CustomForm(),
+                        const Center(
+                          child: Padding(
+                            padding: EdgeInsets.only(top: 20),
+                            child: ConnectButton(),
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                ],
+              ),
             ),
           ),
         ),
--- lib/theme/custom_decoration_effects.dart
+++ lib/theme/custom_decoration_effects.dart
@@ -3,32 +3,40 @@ import 'package:macro_sync_client/theme/exports_theme.dart';
 
 class CustomDecorationEffects {
   final _appColors = AppTheme.colors;
-  final int _lines = 30;
-  final int _lineDivision = 12;
 
   late final diagonalLinearGradientYellow = LinearGradient(
-    colors: List.generate(
-      _lines,
-      (index) => index.isEven
-          ? _appColors.buttonLigthYellowDiagonal
-          : _appColors.buttonDarkYellowDiagonal,
-    ),
-    begin: Alignment.topCenter,
-    end: Alignment.bottomCenter,
-    stops: List.generate(_lines, (index) => index / _lineDivision),
-    tileMode: TileMode.repeated,
-    transform: const GradientRotation(0.785),
-  );
+      colors: List.generate(
+        30,
+        (index) => index.isEven
+            ? _appColors.buttonLigthYellowDiagonal
+            : _appColors.buttonDarkYellowDiagonal,
+      ),
+      begin: Alignment.topCenter,
+      end: Alignment.bottomCenter,
+      stops: List.generate(30, (index) => index / 12),
+      tileMode: TileMode.repeated,
+      transform: const GradientRotation(0.785));
 
   late final diagonalLinearGradientGray = LinearGradient(
-    colors: List.generate(
-      _lines,
-      (index) => index.isEven ? Colors.black : _appColors.borderGray,
-    ),
-    begin: Alignment.topCenter,
-    end: Alignment.bottomCenter,
-    stops: List.generate(_lines, (index) => index / _lineDivision),
-    tileMode: TileMode.repeated,
-    transform: const GradientRotation(0.785),
-  );
+      colors: List.generate(
+        30,
+        (index) => index.isEven ? Colors.black : _appColors.borderGray,
+      ),
+      begin: Alignment.topCenter,
+      end: Alignment.bottomCenter,
+      stops: List.generate(30, (index) => index / 12),
+      tileMode: TileMode.repeated,
+      transform: const GradientRotation(0.785));
+
+  late final verticalLinearGradientGrey = LinearGradient(
+      colors: List.generate(
+        100,
+        (index) =>
+            index.isEven ? Colors.transparent : AppTheme.colors.borderGray,
+      ),
+      begin: Alignment.topCenter,
+      end: Alignment.bottomCenter,
+      stops: List.generate(100, (index) => index / 80),
+      tileMode: TileMode.repeated,
+      transform: const GradientRotation(0.785));
 }
